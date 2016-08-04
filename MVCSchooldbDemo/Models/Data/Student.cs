e8d484898802133556c2859f7bfa02280945258a
LIU Wei <liuwei@nbu.edu.cn>
2016-08-04 22:21:16 +0800

    temp

---
+++ MVCSchooldbDemo/Models/Data/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace MVCSchooldbDemo.Models.Data
+{
+    public class Student
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Sno { get; set; }
+        public string Sname { get; set; }
+        public string Ssex { get; set; }
+        public int Sage { get; set; }
+        public string Sdept { get; set; }
+        public string Sphoto { get; set; }
+    }
+}
